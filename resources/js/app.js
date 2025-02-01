07626c1aea40b31966522803657f9ed7b3f495c3
Jay-R Tillo <tillojayr@gmail.com>
2025-02-01 12:46:22 +0800

    Revert "Merge pull request #1 from tillojayr/01/23/25"
    This reverts commit d0a46de49861c270f7222b11392164c87482b1c6, reversing
    changes made to ac66defc814d6ba38a45a5103a6a80963faa0a84.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
-import "./bootstrap";
+import './bootstrap';
 
-import Alpine from "alpinejs";
+import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
-console.log("app.js");
 Alpine.start();
--- routes/channels.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Broadcast;
-
-Broadcast::channel('testChannel', function () {
-    return true; // Allow anyone to connect to this channel
-});
