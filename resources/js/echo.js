ad50813ed1d9094e891aea8dfdf033dcfecf9ebc
Jay-R <jayr.tillo@nabepero.co.jp>
2025-03-10 16:24:49 +0800

    nginx reverb

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,14 +5,24 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: window.location.hostname,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
-    disableStats: true,
-    cluster: 'mt1',
+    path: '/app/{appKey}' // Make sure this path matches your Nginx config
 });
+
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: 'your-app-key',
+//     wsHost: 'jayrtillo.online',
+//     wsPort: 443,
+//     wssPort: 443,
+//     forceTLS: true,
+//     enabledTransports: ['ws', 'wss'],
+//     path: '/reverb/app/{appKey}' // Make sure this path matches your Nginx config
+// });
 
 // For testing the connection
 window.Echo.connector.pusher.connection.bind('connected', () => {
