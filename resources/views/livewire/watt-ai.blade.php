fe5c1f3397975d4d841e5fcaafe6f5ca2cbe3d6e
Jay-R <jayr.tillo@nabepero.co.jp>
2025-04-23 18:57:40 +0800

    revert

--- resources/views/livewire/watt-ai.blade.php
+++ resources/views/livewire/watt-ai.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="flex items-center justify-center min-h-[60vh]">
         <h1 class="text-6xl font-bold text-gray-700 dark:text-gray-300">
-            <p class="text-electric-orange-800">Watt AI</p> Coming Soon...
+            Watt AI Coming Soon...
         </h1>
     </div>
 </div>
